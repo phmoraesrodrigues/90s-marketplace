8cdec11ec60043f24d40369d6835ad0c5a22f516
Pedro Rodrigues <phmoraesrodrigues@gmail.com>
2021-02-25 18:14:52 -0300

    feat: add real products from CGTrader website

---
+++ src/data.js
@@ -0,0 +1,35 @@
+import picFerrari from './assets/images/ferrari.jpg';
+import picMercedes from './assets/images/mercedes.jpg';
+import picMclaren from './assets/images/mclaren.jpg';
+import picPorsche from './assets/images/porsche.jpg';
+
+export const products = [
+  {
+    id: 1,
+    uid: 'ferrari',
+    name: 'Ferrari LaFerrari 3D model',
+    price: 104.3,
+    picture: picFerrari,
+  },
+  {
+    id: 2,
+    uid: 'mercedes',
+    name: 'Mercedes AMG GT 3D model',
+    price: 52.5,
+    picture: picMercedes,
+  },
+  {
+    id: 3,
+    uid: 'mclaren',
+    name: 'McLaren Artura 2022 3D model',
+    price: 6.99,
+    picture: picMclaren,
+  },
+  {
+    id: 4,
+    uid: 'porsche',
+    name: 'Porsche 911 Turbo S 3D model',
+    price: 118.3,
+    picture: picPorsche,
+  },
+];
