439f367fe05cd93755c9dc4651c8877d205d3b7a
Pedro Rodrigues <phmoraesrodrigues@gmail.com>
2021-02-26 17:42:25 -0300

    feat: wrap links with Router component

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,22 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, BrowserRouter as Router } from "react-router-dom";
 
 const cartItems = () => [];
 
 const Header = () => (
   <header data-testid="header-wrapper">
     <div data-testid="header-logo">90s shop</div>
-    <nav data-testid="header-nav">
-      <ul style={{ listStyleType: "none", display: "flex" }}>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        |
-        <li>
-          <Link to="/cart">Cart ({cartItems().length})</Link>
-        </li>
-      </ul>
-    </nav>
+    <Router>
+      <nav data-testid="header-nav">
+        <ul style={{ listStyleType: "none", display: "flex" }}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          |
+          <li>
+            <Link to="/cart">Cart ({cartItems().length})</Link>
+          </li>
+        </ul>
+      </nav>
+    </Router>
     <hr />
   </header>
 );
