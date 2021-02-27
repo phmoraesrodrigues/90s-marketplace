c4365f1b07871682e8e02429843da2c12f5dfecb
Pedro Rodrigues <phmoraesrodrigues@gmail.com>
2021-02-27 01:23:41 -0300

    style: layout adaptations

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -9,21 +9,19 @@ const { Header: StyledHeader } = Layout;
 const cartItems = () => [];
 
 const Header = () => (
-  <div>
-    <StyledHeader data-testid="header-wrapper" theme="light">
-      <div data-testid="header-logo" className="header-logo">
-        90s Shop
-      </div>
-      <Menu data-testid="header-nav" theme="dark" mode="horizontal">
-        <Menu.Item key="home" title="Home">
-          <Link to="/">Home</Link>
-        </Menu.Item>
-        <Menu.Item key="cart" title="Cart" icon={<ShoppingCartOutlined />}>
-          <Link to="/cart">Cart ({cartItems().length})</Link>
-        </Menu.Item>
-      </Menu>
-    </StyledHeader>
-  </div>
+  <StyledHeader data-testid="header-wrapper" theme="light">
+    <div data-testid="header-logo" className="header-logo">
+      90s Shop
+    </div>
+    <Menu data-testid="header-nav" theme="dark" mode="horizontal">
+      <Menu.Item key="home" title="Home">
+        <Link to="/">Home</Link>
+      </Menu.Item>
+      <Menu.Item key="cart" title="Cart" icon={<ShoppingCartOutlined />}>
+        <Link to="/cart">Cart ({cartItems().length})</Link>
+      </Menu.Item>
+    </Menu>
+  </StyledHeader>
 );
 
 export default Header;
