c501ae357d19b104c762e2994757228d9565781f
Pedro Rodrigues <phmoraesrodrigues@gmail.com>
2021-02-27 00:41:41 -0300

    fix: header routes fixes

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, BrowserRouter as Router } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Layout, Menu } from "antd";
 import { ShoppingCartOutlined } from "@ant-design/icons";
 import "./Header.scss";
@@ -9,15 +9,12 @@ const { Header: StyledHeader } = Layout;
 const cartItems = () => [];
 
 const Header = () => (
-  <StyledHeader data-testid="header-wrapper" theme="light">
-    <div className="header-logo">90s Shop</div>
-    <Router>
-      <Menu
-        data-testid="header-nav"
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={["home"]}
-      >
+  <div>
+    <StyledHeader data-testid="header-wrapper" theme="light">
+      <div data-testid="header-logo" className="header-logo">
+        90s Shop
+      </div>
+      <Menu data-testid="header-nav" theme="dark" mode="horizontal">
         <Menu.Item key="home" title="Home">
           <Link to="/">Home</Link>
         </Menu.Item>
@@ -25,8 +22,8 @@ const Header = () => (
           <Link to="/cart">Cart ({cartItems().length})</Link>
         </Menu.Item>
       </Menu>
-    </Router>
-  </StyledHeader>
+    </StyledHeader>
+  </div>
 );
 
 export default Header;
--- src/layout/Header/Header.test.jsx
+++ src/layout/Header/Header.test.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./Header";
 
-const setup = () => render(<Header />);
+const setup = () =>
+  render(
+    <Router>
+      <Header />
+    </Router>
+  );
 
 describe("when Header load", () => {
   test("should render the wrapper", () => {
@@ -10,13 +16,13 @@ describe("when Header load", () => {
     expect(getByTestId("header-wrapper")).toBeInTheDocument();
   });
 
-  // test("should render the logo", () => {
-  //   const { getByTestId } = setup();
-  //   expect(getByTestId("header-logo")).toBeInTheDocument();
-  // });
-  //
-  // test("should render the nav", () => {
-  //   const { getByTestId } = setup();
-  //   expect(getByTestId("header-nav")).toBeInTheDocument();
-  // });
+  test("should render the logo", () => {
+    const { getByTestId } = setup();
+    expect(getByTestId("header-logo")).toBeInTheDocument();
+  });
+
+  test("should render the nav", () => {
+    const { getByTestId } = setup();
+    expect(getByTestId("header-nav")).toBeInTheDocument();
+  });
 });
