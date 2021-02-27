a9f4d379fe2e7e1da7355341c0cb842079ec7383
Pedro Rodrigues <phmoraesrodrigues@gmail.com>
2021-02-26 23:45:18 -0300

    feat: update footer to use Antd components

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Layout } from "antd";
+import "./Footer.scss";
+
+const { Footer: StyledFooter } = Layout;
 
 const Footer = () => (
-  <footer data-testid="footer-wrapper">
-    <p data-testid="footer-copyright">&copy; Copyright 2021</p>
-  </footer>
+  <StyledFooter data-testid="footer-wrapper">
+    <p data-testid="footer-copyright" className="footer-copyright">
+      &copy; Copyright 2021
+    </p>
+  </StyledFooter>
 );
 
 export default Footer;
