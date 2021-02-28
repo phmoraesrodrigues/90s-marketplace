25650838a77b294740355da17354d6ecd9342d95
Pedro Rodrigues <phmoraesrodrigues@gmail.com>
2021-02-28 14:34:18 -0300

    feat: update title in NotFound page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => (
   <div data-testid="not.found-wrapper">
-    <h3>404 - Not Found</h3>
+    <h2>404 - Not Found</h2>
     <div>
       <Link to="/">Visit Home</Link>
     </div>
--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { useParams, Redirect } from "react-router-dom";
+import products from "../../data/products";
 
-const Product = () => (
-  <div data-testid="product-wrapper">
-    <div>Product</div>
-  </div>
-);
+const Product = () => {
+  const { id } = useParams();
+  const product = products.find((_product) => _product.uid === id);
+
+  if (!product) {
+    return <Redirect to="/404" />;
+  }
+
+  return (
+    <div data-testid="product-wrapper">
+      <div>Product {id}</div>
+    </div>
+  );
+};
 
 export default Product;
