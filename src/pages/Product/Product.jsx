043cbce461d2e57401632a6a8882eb6ea6ab469e
Pedro Rodrigues <phmoraesrodrigues@gmail.com>
2021-02-28 18:51:35 -0300

    feat: implement product details

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,18 +1,49 @@
 import React from "react";
 import { useParams, Redirect } from "react-router-dom";
+import { Row, Col, Image, Button } from "antd";
+import { ShoppingCartOutlined } from "@ant-design/icons";
 import products from "../../data/products";
+import { currencyFormat } from "../../utils/numberUtil";
+import "./Product.scss";
+
+const addToCart = (id) => {
+  console.log(id);
+};
 
 const Product = () => {
-  const { id } = useParams();
-  const product = products.find((_product) => _product.uid === id);
+  const { id: productId } = useParams();
+  const product = products.find((_product) => _product.uid === productId);
 
   if (!product) {
     return <Redirect to="/404" />;
   }
 
+  const { id, name, description, price, picture } = product;
+
   return (
     <div data-testid="product-wrapper">
-      <div>Product {id}</div>
+      <h2 data-testid="product-name">{name}</h2>
+      <Row gutter={16}>
+        <Col xs={24} md={16}>
+          <Image data-testid="product-picture" src={picture} />
+        </Col>
+        <Col className="product-details" xs={24} md={8}>
+          <p data-testid="product-description">{description}</p>
+          <h3>
+            Price: <em data-testid="product-price">{currencyFormat(price)}</em>
+          </h3>
+          <Button
+            data-testid="product-addToCard"
+            onClick={() => addToCart(id)}
+            block
+            size="large"
+            type="primary"
+            icon={<ShoppingCartOutlined />}
+          >
+            Add to cart
+          </Button>
+        </Col>
+      </Row>
     </div>
   );
 };
--- src/pages/Product/Product.test.jsx
+++ src/pages/Product/Product.test.jsx
@@ -1,12 +1,54 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import Product from "./Product";
+import { Route, Switch, MemoryRouter } from "react-router-dom";
 
-const setup = () => render(<Product />);
+import Product from "./Product";
+import products from "../../data/products";
+import { currencyFormat } from "../../utils/numberUtil";
+
+const defaultProduct = (products.length && { ...products[0] }) || {
+  id: 1,
+  uid: "product",
+  name: "Product",
+  description: "Description",
+  price: 15,
+  picture: "about:blank",
+};
+
+const setup = () =>
+  render(
+    <MemoryRouter initialEntries={[`/product/${defaultProduct.uid}`]}>
+      <Switch>
+        <Route path="/product/:id" component={Product} />
+      </Switch>
+    </MemoryRouter>
+  );
 
 describe("when Product load", () => {
   test("should render the wrapper", () => {
     const { getByTestId } = setup();
     expect(getByTestId("product-wrapper")).toBeInTheDocument();
+  });
+
+  test("should render the name", () => {
+    const { getByTestId } = setup();
+    expect(getByTestId("product-name")).toBeInTheDocument();
+  });
+
+  test("should render the picture", () => {
+    const { getByTestId } = setup();
+    expect(getByTestId("product-picture")).toBeInTheDocument();
+  });
+
+  test("should render the price", () => {
+    const { getByTestId } = setup();
+    expect(getByTestId("product-price")).toBeInTheDocument();
+  });
+
+  test("should render the price in currency format", () => {
+    const { getByTestId } = setup();
+    expect(getByTestId("product-price").textContent).toEqual(
+      currencyFormat(defaultProduct.price)
+    );
   });
 });
