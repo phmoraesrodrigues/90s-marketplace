3fcc0cdfed5f895372f8da6dbe025f002ef823d9
Pedro Rodrigues <phmoraesrodrigues@gmail.com>
2021-02-28 14:10:11 -0300

    feat: add products to Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Row, Col } from "antd";
+import ProductCard from "../../components/ProductCard";
+import products from "../../data/products";
 
 const Home = () => (
   <div data-testid="home-wrapper">
-    <div>Welcome to our shop!</div>
+    <h2>Welcome to our shop!</h2>
+    <Row gutter={16}>
+      {products.map(({ id, uid, name, price, picture }) => (
+        <Col key={id} xs={24} sm={12} md={6}>
+          <ProductCard uid={uid} name={name} price={price} picture={picture} />
+        </Col>
+      ))}
+    </Row>
   </div>
 );
 
--- src/pages/Home/Home.test.jsx
+++ src/pages/Home/Home.test.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./Home";
+import products from "../../data/products";
 
-const setup = () => render(<Home />);
+const setup = () =>
+  render(
+    <Router>
+      <Home />
+    </Router>
+  );
 
 describe("when Home load", () => {
   test("should render the wrapper", () => {
     const { getByTestId } = setup();
     expect(getByTestId("home-wrapper")).toBeInTheDocument();
+  });
+
+  test("should render the products", () => {
+    const { getAllByTestId } = setup();
+    expect(getAllByTestId("product.card-wrapper").length).toEqual(
+      products.length
+    );
   });
 });
