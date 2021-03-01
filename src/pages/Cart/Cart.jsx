14fb1bd76c0fcbedc53afb3b3bb21465b2ea3b8c
Pedro Rodrigues <phmoraesrodrigues@gmail.com>
2021-03-01 02:23:50 -0300

    feat: implement Cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,9 +1,127 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Row, Col, Empty, Button, Card, notification, Space } from "antd";
+import { Link } from "react-router-dom";
+import { RightCircleOutlined, DeleteOutlined } from "@ant-design/icons";
+import CartItem from "./CartItem";
+import products from "../../data/products";
+import { currencyFormat } from "../../utils/numberUtil";
+import cartTypes from "../../store/types/cartType";
 
-const Cart = () => (
-  <div data-testid="cart-wrapper">
-    <div>Cart</div>
-  </div>
+const totalValue = (cartItems) =>
+  cartItems.reduce((total, item) => {
+    const { price } = products.find((product) => product.id === item);
+    return total + price;
+  }, 0);
+
+const openNotification = () => {
+  notification.open({
+    message: "Not Implemented",
+    description: "Soon we'll be accepting payments!",
+  });
+};
+
+const renderEmptyCart = () => (
+  <>
+    <Empty
+      data-testid="cart-empty.state"
+      image={Empty.PRESENTED_IMAGE_SIMPLE}
+      description={<span>Your shopping cart is empty.</span>}
+    >
+      <Link to="/">
+        <Button size="large" type="primary">
+          Browse Products
+        </Button>
+      </Link>
+    </Empty>
+  </>
 );
+
+const renderItems = (cartItems, clearCart, removeFromCart) => {
+  return (
+    <>
+      <h2>
+        <Space>
+          Cart (
+          <em data-testid="cart-count.items">
+            {cartItems.length} product{cartItems.length !== 1 && "s"}
+          </em>
+          )
+          <Button
+            data-testid="cart-clear"
+            type="primary"
+            ghost
+            size="small"
+            onClick={() => clearCart()}
+          >
+            <DeleteOutlined /> Clear Cart
+          </Button>
+        </Space>
+      </h2>
+      <Row gutter={16}>
+        <Col xs={24} md={16}>
+          {cartItems.map((item) => {
+            const { uid, name, price, picture } = products.find(
+              (product) => product.id === item
+            );
+            return (
+              <CartItem
+                key={item}
+                id={item}
+                uid={uid}
+                name={name}
+                price={price}
+                picture={picture}
+                removeFromCart={removeFromCart}
+              />
+            );
+          })}
+        </Col>
+        <Col xs={24} md={8}>
+          <Card data-testid="cart-summary">
+            <h3>Order Summary</h3>
+            <h4>
+              Total:{" "}
+              <em data-testid="cart-total.price">
+                {currencyFormat(totalValue(cartItems))}
+              </em>
+            </h4>
+            <Button
+              data-testid="product-continueToCheckout"
+              size="large"
+              type="primary"
+              block
+              onClick={openNotification}
+              icon={<RightCircleOutlined />}
+            >
+              Continue to Checkout
+            </Button>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+const Cart = () => {
+  const cartItems = useSelector((state) => state.cart.items);
+  const cartLength = cartItems.length;
+  const dispatch = useDispatch();
+
+  const clearCart = () => {
+    dispatch({ type: cartTypes.CLEAR_CART });
+  };
+
+  const removeFromCart = (itemId) => {
+    dispatch({ type: cartTypes.REMOVE_FROM_CART, payload: itemId });
+  };
+
+  return (
+    <div data-testid="cart-wrapper">
+      {!cartLength ? renderEmptyCart() : null}
+      {cartLength ? renderItems(cartItems, clearCart, removeFromCart) : null}
+    </div>
+  );
+};
 
 export default Cart;
--- src/pages/Cart/Cart.test.jsx
+++ src/pages/Cart/Cart.test.jsx
@@ -1,12 +1,83 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import rootReducer from "../../store/reducers";
 import Cart from "./Cart";
+import { currencyFormat } from "../../utils/numberUtil";
+import products from "../../data/products";
 
-const setup = () => render(<Cart />);
+const defaultProduct = (products.length && { ...products[0] }) || {
+  id: 1,
+  uid: "product",
+  name: "Product",
+  description: "Description",
+  price: 15,
+  picture: "about:blank",
+};
+
+const initialState = {
+  cart: {
+    items: [defaultProduct.id],
+  },
+};
+
+const setup = (items = null) => {
+  const state = items
+    ? { ...initialState, cart: { items } }
+    : { ...initialState };
+
+  const store = createStore(rootReducer, state);
+  return render(
+    <Provider store={store}>
+      <Router>
+        <Cart />
+      </Router>
+    </Provider>
+  );
+};
 
 describe("when Cart load", () => {
   test("should render the wrapper", () => {
     const { getByTestId } = setup();
     expect(getByTestId("cart-wrapper")).toBeInTheDocument();
+  });
+
+  test("should render the empty state with empty cart", () => {
+    const { getByTestId } = setup([]);
+    expect(getByTestId("cart-empty.state")).toBeInTheDocument();
+  });
+
+  test("should render the items count", () => {
+    const { getByTestId } = setup();
+    expect(getByTestId("cart-count.items")).toBeInTheDocument();
+  });
+
+  test("should render the correct items count value", () => {
+    const { getByTestId } = setup();
+    expect(getByTestId("cart-count.items").textContent).toEqual("1 product");
+  });
+
+  test("should render the clear button", () => {
+    const { getByTestId } = setup();
+    expect(getByTestId("cart-clear")).toBeInTheDocument();
+  });
+
+  test("should render the order summary", () => {
+    const { getByTestId } = setup();
+    expect(getByTestId("cart-summary")).toBeInTheDocument();
+  });
+
+  test("should render the price", () => {
+    const { getByTestId } = setup();
+    expect(getByTestId("cart-total.price")).toBeInTheDocument();
+  });
+
+  test("should render the total price in currency format", () => {
+    const { getByTestId } = setup();
+    expect(getByTestId("cart-total.price").textContent).toEqual(
+      currencyFormat(defaultProduct.price)
+    );
   });
 });
