84133800d4a3e92c5cde7e0c1a9637194fafb197
Pedro Rodrigues <phmoraesrodrigues@gmail.com>
2021-03-01 00:06:43 -0300

    feat: add store provider to App test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import rootReducer from "./store/reducers";
 
-const setup = () =>
-  render(
-    <Router>
-      <App />
-    </Router>
+const initialState = {
+  cart: {
+    items: [],
+  },
+};
+
+const setup = () => {
+  const store = createStore(rootReducer, initialState);
+  return render(
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   );
+};
 
 describe("when App load", () => {
   test("should render the wrapper", () => {
