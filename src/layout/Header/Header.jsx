cf18dad54d2964d451084ee75c936128a373f1d7
Pedro Rodrigues <phmoraesrodrigues@gmail.com>
2021-03-01 00:07:49 -0300

    feat: integrate cart store to Header

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,32 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { Layout, Menu } from "antd";
 import { ShoppingCartOutlined } from "@ant-design/icons";
 import "./Header.scss";
 
 const { Header: StyledHeader } = Layout;
 
-const cartItems = () => [];
+const Header = () => {
+  const cartItems = useSelector((state) => state.cart.items);
 
-const Header = () => (
-  <StyledHeader data-testid="header-wrapper" theme="light">
-    <div data-testid="header-logo" className="header-logo">
-      90s Shop
-    </div>
-    <Menu
-      data-testid="header-nav"
-      theme="dark"
-      selectable={false}
-      mode="horizontal"
-    >
-      <Menu.Item key="home" title="Home">
-        <Link to="/">Home</Link>
-      </Menu.Item>
-      <Menu.Item key="cart" title="Cart" icon={<ShoppingCartOutlined />}>
-        <Link to="/cart">Cart ({cartItems().length})</Link>
-      </Menu.Item>
-    </Menu>
-  </StyledHeader>
-);
+  return (
+    <StyledHeader data-testid="header-wrapper" theme="light">
+      <div data-testid="header-logo" className="header-logo">
+        90s Shop
+      </div>
+      <Menu
+        data-testid="header-nav"
+        theme="dark"
+        selectable={false}
+        mode="horizontal"
+      >
+        <Menu.Item key="home" title="Home">
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item key="cart" title="Cart" icon={<ShoppingCartOutlined />}>
+          <Link to="/cart">Cart ({cartItems.length})</Link>
+        </Menu.Item>
+      </Menu>
+    </StyledHeader>
+  );
+};
 
 export default Header;
--- src/layout/Header/Header.test.jsx
+++ src/layout/Header/Header.test.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
+import rootReducer from "../../store/reducers";
 import Header from "./Header";
 
-const setup = () =>
-  render(
-    <Router>
-      <Header />
-    </Router>
+const initialState = {
+  cart: {
+    items: [],
+  },
+};
+
+const setup = () => {
+  const store = createStore(rootReducer, initialState);
+  return render(
+    <Provider store={store}>
+      <Router>
+        <Header />
+      </Router>
+    </Provider>
   );
+};
 
 describe("when Header load", () => {
   test("should render the wrapper", () => {
