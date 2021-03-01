a7422b1cff4e120e48114ad0076b0725ec563f6c
Pedro Rodrigues <phmoraesrodrigues@gmail.com>
2021-02-28 22:55:14 -0300

    feat: move Router to index

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 import { Layout } from "antd";
 import Content from "./layout/Content";
 import Footer from "./layout/Footer";
@@ -7,13 +6,11 @@ import Header from "./layout/Header";
 
 const App = () => {
   return (
-    <Router>
-      <Layout data-testid="app-wrapper">
-        <Header />
-        <Content />
-        <Footer />
-      </Layout>
-    </Router>
+    <Layout data-testid="app-wrapper">
+      <Header />
+      <Content />
+      <Footer />
+    </Layout>
   );
 };
 
