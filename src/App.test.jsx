9d3788d8e46df5f60634239abf243d4692246eef
Pedro Rodrigues <phmoraesrodrigues@gmail.com>
2021-02-28 23:50:54 -0300

    feat: add Router to App test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
-const setup = () => render(<App />);
+const setup = () =>
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
 
 describe("when App load", () => {
   test("should render the wrapper", () => {
