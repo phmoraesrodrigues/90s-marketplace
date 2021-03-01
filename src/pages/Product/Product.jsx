04ebeb983bd3d97f7c5315e693856d46e0d0e75e
Pedro Rodrigues <phmoraesrodrigues@gmail.com>
2021-03-01 02:23:17 -0300

    feat: button switch between Add to Cart and Proceed to Checkout on Product

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { useParams, Redirect } from "react-router-dom";
+import { useParams, Redirect, Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import { Row, Col, Image, Button } from "antd";
-import { ShoppingCartOutlined } from "@ant-design/icons";
+import { ShoppingCartOutlined, RightCircleOutlined } from "@ant-design/icons";
+import cartTypes from "../../store/types/cartType";
 import products from "../../data/products";
 import { currencyFormat } from "../../utils/numberUtil";
 import "./Product.scss";
 
-const addToCart = (id) => {
-  console.log(id);
-};
-
 const Product = () => {
+  const cartItems = useSelector((state) => state.cart.items);
+  const dispatch = useDispatch();
   const { id: productId } = useParams();
   const product = products.find((_product) => _product.uid === productId);
 
@@ -19,6 +19,37 @@ const Product = () => {
   }
 
   const { id, name, description, price, picture } = product;
+
+  const isItemAddedToCart = cartItems.indexOf(id) !== -1;
+
+  const addToCart = (itemId) => {
+    dispatch({ type: cartTypes.ADD_TO_CART, payload: itemId });
+  };
+
+  const itemButton = isItemAddedToCart ? (
+    <Link to="/cart">
+      <Button
+        data-testid="product-proceedToCheckout"
+        block
+        size="large"
+        type="primary"
+        icon={<RightCircleOutlined />}
+      >
+        Proceed to checkout
+      </Button>
+    </Link>
+  ) : (
+    <Button
+      data-testid="product-addToCard"
+      onClick={() => addToCart(id)}
+      block
+      size="large"
+      type="primary"
+      icon={<ShoppingCartOutlined />}
+    >
+      Add to cart
+    </Button>
+  );
 
   return (
     <div data-testid="product-wrapper">
@@ -32,16 +63,7 @@ const Product = () => {
           <h3>
             Price: <em data-testid="product-price">{currencyFormat(price)}</em>
           </h3>
-          <Button
-            data-testid="product-addToCard"
-            onClick={() => addToCart(id)}
-            block
-            size="large"
-            type="primary"
-            icon={<ShoppingCartOutlined />}
-          >
-            Add to cart
-          </Button>
+          {itemButton}
         </Col>
       </Row>
     </div>
